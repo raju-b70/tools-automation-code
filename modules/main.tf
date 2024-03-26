058cd6ceeab18c48e20ca29ae0b8544caf6acad8
rajendra <rajendrammarboyana@gmail.com>
2024-03-26 12:52:55 +0000

    creating iam role for prometheus

--- modules/main.tf
+++ modules/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+  iam_instance_profile = ""
 
   tags = {
     Name = var.tool_name
@@ -16,3 +17,28 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.public_ip]
   ttl     = 30
 }
+
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}
+
+
+
