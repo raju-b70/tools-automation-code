a87ca50d406eda980157cbf144803b3217e7c1af
rajendra <rajendrammarboyana@gmail.com>
2024-03-28 13:28:35 +0000

    enabling spot instances

--- modules/main.tf
+++ modules/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [data.aws_security_group.selected.id]
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
+  spot options {
+    instance_interruption_behaviour = "stop"
+    spot_instance_type              = "persistent"
+  }
+
   tags = {
     Name = var.tool_name
     monitor = "yes"
