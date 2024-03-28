647fad37fd9c043aae96a6d9fb7f20cbe31efcc0
rajendra <rajendrammarboyana@gmail.com>
2024-03-28 10:19:30 +0000

    adding labels in prometheus and tags in terraform

--- modules/main.tf
+++ modules/main.tf
@@ -3,10 +3,7 @@ resource "aws_instance" "instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-  spot_options{
-    instance_interruption_behaviour = "stop"
-    spot_instance_type = "persistent"
-  }
+
 
   tags = {
     Name = var.tool_name
