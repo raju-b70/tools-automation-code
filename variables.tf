3e0ae95198c2d7795a36a318835e137fcde9f685
rajendra <rajendrammarboyana@gmail.com>
2024-03-28 10:16:36 +0000

    adding labels in prometheus and tags in terraform

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "tools" {
   default = {
     prometheus = {
-      instance_type = "t3.small"
+      instance_type        = "t3.small"
       policy_resource_list = ["ec2:DescribeInstances"]
 
-    grafana  = {
-      instance_type = "t3.small"
-      policy_resource_list = ["ec2:DescribeInstances"]
+      grafana = {
+        instance_type        = "t3.small"
+        policy_resource_list = ["ec2:DescribeInstances"]
 
+      }
     }
   }
 }
-
 
 variable "zone_id" {
   default = "Z07215953NITTL12YPSW3"
